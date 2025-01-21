d42eef9dbec8433cce6c883ecbda184b282afb63
Heliferreira <heliveltonsouza@outlook.com>
2025-01-20 21:20:51 -0300

    projeto plataforma de cursos

---
+++ tela de login/bdConnect.js
@@ -0,0 +1,5 @@
+class connection {
+    hostname
+    endereco
+    senha
+}
---
+++ tela de login/login.js
@@ -0,0 +1,13 @@
+class login {
+    var email;
+    var senha;
+    myssql.connection (ipBanco, hostname)
+
+    if(email){
+        this.email = email;
+        this.senha = senha;
+
+        //senha existe;
+        //senha acessa;
+    }
+}
